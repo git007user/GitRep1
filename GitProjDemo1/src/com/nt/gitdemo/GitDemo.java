b314571fab6ec2345aed26521250f2b80fd3eb29
dev1 <rames@192.168.0.4>
2018-06-13 22:17:36 +0530

    dev-added one java file

---
+++ GitProjDemo1/src/com/nt/gitdemo/GitDemo.java
@@ -0,0 +1,9 @@
+package com.nt.gitdemo;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		System.out.println("getDemo test");
+	}
+
+}
--- GitProjDemo1/src/com/nt/gitdemo/GitTest.java
+++ GitProjDemo1/src/com/nt/gitdemo/GitTest.java
@@ -4,6 +4,9 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("welcome to git");
+		
+		
+		System.out.println("hello world");
 
 	}
 
